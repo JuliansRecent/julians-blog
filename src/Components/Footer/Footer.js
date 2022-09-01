91e1f5ffe4e9263d948488c935f3c3821775388b
JuliansRecent <juliansrecent@gmail.com>
2022-09-01 00:06:31 -0700

    Make code formatting more uniform.

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import './Footer.css'
+import './Footer.css';
 
 function Footer(props) {
-    return (
-			<div className='footer-body d-flex justify-content-between'>
-				<span className='footer-text'><a href="">Julian's Blog</a></span>
-				<div>
-					<ul className='d-flex gap-2'>
-						<li>
-							<a href='https://www.julian-segura12.com/'>Portfolio </a>
-						</li>
-						<li className='ml-1'>
-							<a href='https://www.linkedin.com/in/julian-segura12/'>
-								· LinkedIn
-							</a>
-						</li>
-						<li className='ml-1'>
-							<a href='mailto:juliansrecent@gmail.com'>· Email</a>
-						</li>
-					</ul>
-				</div>
+	return (
+		<div className='footer-body d-flex justify-content-between'>
+			<span className='footer-text'>
+				<a href=''>Julian's Blog</a>
+			</span>
+			<div>
+				<ul className='d-flex gap-2'>
+					<li>
+						<a href='https://www.julian-segura12.com/'>Portfolio </a>
+					</li>
+					<li className='ml-1'>
+						<a href='https://www.linkedin.com/in/julian-segura12/'>
+							· LinkedIn
+						</a>
+					</li>
+					<li className='ml-1'>
+						<a href='mailto:juliansrecent@gmail.com'>· Email</a>
+					</li>
+				</ul>
 			</div>
-		);
+		</div>
+	);
 }
 
 export default Footer;
--- src/Components/Main/LandingPage/LandingPage.js
+++ src/Components/Main/LandingPage/LandingPage.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function LandingPage(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+	return <div></div>;
 }
 
 export default LandingPage;
